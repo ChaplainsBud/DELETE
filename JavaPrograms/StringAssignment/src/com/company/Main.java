4ad0b79bab9574de01b5d523ca1d911300b4da66
Matthew <matthew.robert.leroe@gmail.com>
2020-01-28 21:30:19 -0500

    First

---
+++ JavaPrograms/StringAssignment/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name = "Matthew";
+        System.out.println(name);
+    }
+}
+
+//Write a Java program that assigns your name to a variable and prints the variable to the console.
